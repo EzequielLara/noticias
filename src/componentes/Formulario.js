87b71093ee33cce981fe1c1e602076ecdd28caa8
EzequielLara <ezk2442rgmail.com>
2020-08-09 16:27:22 +0200

    Creando un custom hook

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import style from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = ()=>{
+
+    // utilizar custom hook
+    const [categoria, SelectNoticias] = useSelect();
     return(
        <div className={`${style.buscador} row`}>
            <div className="col s12 m8 offset-m2">
                <form>
                    <h2 className={style.heading}>Encuentra Noticias por Categoria</h2>
+                   <SelectNoticias />
                    <div className="input-field col s12">
                        <input
                             type="submit"
