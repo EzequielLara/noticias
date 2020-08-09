1997c60c56b649ce578cf6f3494e64cbfc838b0e
EzequielLara <ezk2442rgmail.com>
2020-08-09 19:28:59 +0200

    Ajustando las opciones del select

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -4,8 +4,18 @@ import useSelect from '../hooks/useSelect';
 
 const Formulario = ()=>{
 
+    const OPCIONES =[
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'sciencie', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnología'}
+    ]
+
     // utilizar custom hook
-    const [categoria, SelectNoticias] = useSelect();
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
     return(
        <div className={`${style.buscador} row`}>
            <div className="col s12 m8 offset-m2">
--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -3,11 +3,15 @@ import React, {useState} from 'react';
 const useSelect = (stateInicial, opciones) =>{
 
     //state del custom hook
-    const [state, actualizarState]=useState('');
+    const [state, actualizarState]=useState(stateInicial);
 
     const SelectNoticias =() =>(
-        <select className="browser-default">
-            <option value="">Seleccione</option>
+        <select
+             className="browser-default"
+             value = {state}
+             onChange={e=> actualizarState(e.target.value)}
+             >
+            {opciones.map(opcion =>(<option key={opcion.value} value={opcion.value}>{opcion.label}</option>))}
         </select>
     );
 
